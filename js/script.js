ff212a6ce614c63aa4c69bf5caa94406c0e64015
Paulo Maia <pmanzonemaia@gmaill.com>
2017-12-19 21:12:42 -0200

    Simplex implementado, falta correcoes menores

---
+++ js/script.js
@@ -0,0 +1,320 @@
+//====================================================================================
+//						Variaveis do simplex
+//====================================================================================
+
+var matriz = []; 	//Matriz das restrições
+var sinais = [];	//Vetor dos sinais das restrições
+var b = []; 		//Vetor b
+var ba = []; 		//Vetor b/a
+var custo = []; 	//Vetor dos custos da função objetivo
+var cr = []; 		//Vetor do custo reduzido
+var menorPositivo; 	//Valor do menor b/a positivo
+var maisNegativo;	//Valor do cr mais negativo
+var tipo;			//0) minimiza 1) maximiza
+var i;				// Auxiliar para linha
+var j;				// Auxiliar para coluna
+var m;				//Numero de restricoes
+var n;				//Numero de variaveis
+var basicas = [];	//Vetor que indica as variáveis que estão na base
+var artificial = [];		//Vetor artificiais
+var sobra = [];		//Vetor das variáveis de excesso
+var infinito = 999999999999;
+var indice;
+var pivoi, pivoj, pivo;
+//====================================================================================
+//						
+//====================================================================================
+
+
+$(document).ready(function(){
+	var restricoes, variaveis, inserir;
+
+	//========================================================================Botão Criar Grade===============================================
+	$("#botaograde").click(function(){
+		restricoes = $("#r").val();
+		variaveis = $("#v").val();
+		tipo = $("#tipo").val();
+
+		if(restricoes>9 | variaveis>9){
+			alert("Digite valores abaixo de 9 nesses campos");
+			if(variaveis>9)$("#v").focus();
+			if(restricoes>9)$("#r").focus();
+			return false;
+		}
+		if(!isNumber(restricoes) || !isNumber(variaveis)){
+			alert("Digite numeros!");
+			if(restricoes>9)$("#r").focus();
+			return false;
+		}
+
+		$("#tabela").empty();
+		if(tipo==10) $("#tabela").append("<p>Maximizar</p><hr>");
+		if(tipo==0)  $("#tabela").append("<p>Minimizar</p><hr>");
+		//Desenhando Z
+		$("#tabela").append("Z = ");
+		for(i=1; i<=variaveis; i++){
+			inserir ='<input type="text" value="0" id ="z' + i + '" class="valoresx"> *x' + i + ' + ';
+			if(i==variaveis) inserir = '<input type="text" value="0" id="z'+ i + '" class="valoresx"> *x' + i + '';
+			$("#tabela").append(inserir);
+		}
+		$("#tabela").append('<br><hr><br>s.a');
+		
+
+		//Desenhando tabela
+		for(i=1; i<=restricoes; i++){
+			$("#tabela").append('<br><br>');
+			for(j=1; j<=variaveis; j++){
+				inserir='<input type="text" value="0" class="valoresx" id="x'+ i + ''+ j + '"> *x[' + j + ']  ';
+				$("#tabela").append(inserir);
+			}
+			$("#tabela").append('<select class="valoresx" id= s'+ i + '> <option value="<="><=</option><option value="=">=</option><option value=">=">>=</option></select>  ');
+			$("#tabela").append('<input type="text" value="0" class="valoresx" id="b'+ i + '">');
+		}
+
+		$("#tabela").append('<br><br><hr>');
+		$("#tabela").append('<input class ="btnbaixo" type="button" id="botaocalcula" name="calcula" value="Calcular"> ');
+		$("#tabela").append('<input class ="btnbaixo" type="button" id="botaolimpa" name="limpa" value="Limpar"> ');
+
+	});
+
+
+	//=======================================================================Botão Limpar==========================================================
+
+	$(document).on('click','#botaolimpa',function(){
+
+		$("#botaograde").trigger('click');
+	});
+
+	//======================================================================Botão ir===========================================================
+	$(document).on('click','#botaocalcula',function(){
+		chamada();
+	});
+
+}); 
+
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+//==============================================================================================================================================
+//==============================================================================================================================================
+//========================================================			MÉTODOS			============================================================
+//==============================================================================================================================================
+//==============================================================================================================================================
+
+
+//==============================================================================================================================================
+//																Função simplex simples
+//==============================================================================================================================================
+
+function simplex(){
+	//Le numero restricoes e variaveis
+	m = $("#r").val();
+	n = $("#v").val();
+	basicas[0] = 0;
+	artificial[0] = 0;
+	sobra[0] = 0;
+	tipo = $('#tipo').val();
+	// Le matriz
+	for(i=1; i<=m; i++){
+		matriz[i] = [];
+		for(j=1; j<=n; j++){
+			matriz[i][j] = $('#x' + i + '' + j).val();
+		}
+	}
+
+	//Le b e sinais
+
+	for(i=1; i<=m; i++){
+		b[i] = $('#b'+i).val();
+		sinais[i] = $('#s' + i).val(); 
+	}
+
+	//Le custo função objetivo
+
+	for(j=1; j<=n; j++){
+		custo[j] = $('#z' + j).val();
+	}
+
+	//Colocando em forma padrão
+
+	//Se for maximizar
+	if(tipo==10){
+		for(j=1; j<=n; j++){
+			custo[j] *=-1;
+		}
+	}
+
+	//Verificando b negativo
+	for(i=1; i<=m; i++){
+		if(b[i]<0){
+			b[i]*=-1;
+			for(j=1; j<=n; j++){
+				matriz[i][j]*=-1;
+			}
+			if(sinais[i] == "<=") sinais[i] = ">=";
+			else if(sinais[i] == ">=") sinais[i] = "<=";
+		}
+	}
+
+	//adicionando variáveis extras
+
+	for(i=1; i<=m; i++){
+		//SINAL <=
+		if(sinais[i] == "<="){
+			sobra[sobra.length] = (++n);
+			custo[n] = 0;
+			for(j=1; j<i; j++){
+				matriz[j][n] = 0;
+			}
+			matriz[i][n] = 1;
+			for(j=i+1; j<=m; j++){
+				matriz[j][n] = 0;
+			}
+		}
+		basicas[basicas.length] = n;
+
+		//SINAL =
+
+		if(sinais[i]=="="){
+			artificial[artificial.length] = (++n);
+			custo[n] = infinito;
+			for(j=1; j<i; j++){
+				matriz[j][n] = 0;
+			}
+			matriz[i][n] = 1;
+			for(j=i+1; j<=m; j++){
+				matriz[j][n] = 0;
+			}
+			basicas[basicas.length] = n;
+		}
+
+		//SINAL >=
+		if(sinais[i] == ">="){
+			custo[custo.length] = 0;
+			custo[custo.length] = infinito;
+			sobra[sobra.length] = (++n);
+			artificial[artificial.length] = (++n);
+			for(j=1; j<i; j++){
+				matriz[j][n]= 0;
+				matriz[j][n-1] = 0;
+			}
+			matriz[i][n] = 1;
+			matriz[i][n-1] =-1;
+
+			for(j=i+1; j<=m; j++){
+				matriz[j][n] = 0;
+				matriz[j][n-1] = 0;
+			}
+			basicas[basicas.length] = n;
+		}
+
+	}
+
+
+	//Resolvendo
+	do{
+		//calculo do cr
+		for(j=1; j<=n; j++){
+			var soma = custo[j];
+			for(i=1; i<=m; i++){
+				soma -= custo[basicas[i]] * matriz[i][j];
+			}
+			cr[j] = soma;
+		}
+
+		//calculo do mais negativo
+		maisNegativo = infinito;
+		indice = 0
+		for(j=1; j<=n; j++){
+			if(cr[j]<maisNegativo && cr[j]<0){
+			 	maisNegativo = cr[j];
+				indice = j;
+			}
+		}
+		if(!indice){
+			alert("B");
+			//verificaCond();
+			return;
+		}
+
+		//calcular b/a
+		
+		for(i=1; i<=m; i++){
+			ba[i] = b[i]/ matriz[i][indice];
+		}
+
+		//achar menor positivo
+		
+		menorPositivo = infinito;
+		pivoi = 0;
+		for(i=1; i<=m; i++){
+			if(ba[i]>=0 && ba[i]<menorPositivo){
+				menorPositivo = ba[i];
+				pivoi = i;
+			}
+		}
+		pivoj = indice;
+
+		if(!pivoi){
+			alert("A");
+			// faz alguma coisa aqui -=-=--=
+			return;
+		}
+		
+
+		//pivotamento
+		pivo = matriz[pivoi][pivoj]
+		for(j=1; j<=n; j++){
+			matriz[pivoi][j] = matriz[pivoi][j]/pivo; 
+		}
+
+		for(i=1; i<=m; i++){
+			pivo = matriz[i][pivoj];
+			for(j=1; j<=n && i!=pivoi; j++){
+				matriz[i][j] -= matriz[pivoi][j] * pivo;
+			}
+		}
+
+	}while(true);
+
+	
+
+
+
+	console.log("custo");
+	console.log(custo);
+	console.log("basicas");
+	console.log(basicas);
+	console.log("matriz");
+	console.log(matriz);
+	console.log("cr");
+	console.log(cr);
+	console.log("artificial");
+	console.log(artificial);
+	console.log("sobra");
+	console.log(sobra);
+
+}
+
+	//Mostrando resultado
+	console.log("Resultado: ")
+	for(i=1; i<=basicas.length; i++){
+		console.log(b[basicas[i]]);
+	}
+
+
+//==============================================================================================================================================
+//																Função simplex 2 fases
+//==============================================================================================================================================
+
+
+//==============================================================================================================================================
+//																Função Chamada
+//==============================================================================================================================================
+
+
+function chamada(){
+	simplex();
+}
